229da769ec47de34d9959f4b792fcd33a04ca8b4
DineshMSDian <dinesh2742004@gmail.com>
2026-06-15 15:07:38 +0530

    Model Training: Naive Prediction: Adjusted train test ratio to 80:20, and fixed a correct test datasets for MAE
    Signed-off-by: DineshMSDian <dinesh2742004@gmail.com>
    
    (x_test, y_pred) -> (y_test, y_pred)

--- linear_regression.py
+++ linear_regression.py
@@ -23,7 +23,7 @@ x = df[['mileage']]
 y = df[['price']]
 
 #2. Train/Test split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 print(x_train.shape, x_test.shape)  
 
 # 3. Fit LinearRegression
@@ -32,5 +32,5 @@ model.fit(x_train, y_train)
 
 # 4. Calculate MAE
 y_pred = model.predict(x_test)
-mae = mean_absolute_error(x_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
 print(mae.__round__())
